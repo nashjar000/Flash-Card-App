4e30dd9e5ab33c23cf8d27c3e3c92d50fc4573b7
Jared Nash <104384562+nashjar000@users.noreply.github.com>
2024-06-27 16:24:55 -0600

    Initial Commit

---
+++ Flash Card App/Flash_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_Card_AppApp.swift
+//  Flash Card App
+//
+//  Created by Jared Nash on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Flash_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
